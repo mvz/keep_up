c251a8a010552148f0c1359bdced88c42b3dcea5
Matijs van Zuijlen <matijs@matijs.net>
2016-11-08 15:59:36 +0100

    Implement basic do-nothing scenario

---
+++ features/step_definitions/basic_step.rb
@@ -0,0 +1,15 @@
+Given(/^a Gemfile specifying:$/) do |string|
+  contents = "source 'https://rubygems.org'\npath 'libs'\n\n#{string}"
+  write_file 'Gemfile', contents
+end
+
+Given(/^a gem named "([^']+)" at version ([0-9.]+)$/) do |gemname, version|
+  base_path = "libs/#{gemname}-#{version}"
+  spec = Gem::Specification.new do |s|
+    s.name = gemname
+    s.version = version
+    s.authors = ['John Doe']
+  end
+  write_file "#{base_path}/#{gemname}.gemspec", spec.to_ruby
+  write_file "#{base_path}/lib/#{gemname}.rb", "true"
+end
---
+++ features/support/env.rb
@@ -0,0 +1,5 @@
+Before do
+  delete_environment_variable 'RUBYOPT'
+  delete_environment_variable 'BUNDLE_BIN_PATH'
+  delete_environment_variable 'BUNDLE_GEMFILE'
+end
