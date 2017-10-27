e8b0d60d56dc9bfc250dc3869fa5306340dff2d4
Matijs van Zuijlen <matijs@matijs.net>
2017-10-27 09:52:25 +0200

    Autocorrect RSpec/DescribedClass

--- spec/keep_up/dependency_spec.rb
+++ spec/keep_up/dependency_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe KeepUp::Dependency do
   describe '#locked_version' do
     it 'normalizes the version to a Gem::Version' do
-      dep = KeepUp::Dependency.new(name: 'foo',
+      dep = described_class.new(name: 'foo',
                                    locked_version: '0.5.2',
                                    requirement_list: [])
       expect(dep.locked_version).to eq Gem::Version.new '0.5.2'
