89b7ffe3c4f1873bf82dab45aa88b33105096b9b
Matijs van Zuijlen <matijs@matijs.net>
2022-07-24 11:09:33 +0200

    Let Ruby resolve require paths in project lib during tests
    Since bin/keep_up now uses require, Ruby needs to be able to resolve the
    location of the main keep_up.rb file inside the project's lib/
    directory. Normally, Bundler handles this, but inside the Aruba
    scenario's we run keep_up outside of the bundle.

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@
 require "rubygems/package"
 
 Before do
+  # Set Aruba environment to match unbundled environment
   empty_env = with_environment { ::Bundler.with_unbundled_env { ENV.to_h } }
   aruba_env = aruba.environment.to_h
   (aruba_env.keys - empty_env.keys).each do |key|
@@ -11,4 +12,8 @@ Before do
   empty_env.each do |k, v|
     set_environment_variable k, v
   end
+
+  # Set RUBYLIB so Ruby can find project's lib/ without Bundler
+  project_lib = File.expand_path("../../lib", __dir__)
+  set_environment_variable("RUBYLIB", project_lib)
 end
