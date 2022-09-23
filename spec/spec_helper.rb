fc76d250d4ff9390166ef5e4995fc681ebaee21f
Matijs van Zuijlen <matijs@matijs.net>
2022-09-23 08:29:20 +0200

    Set up rspec to make rspec -n work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@
 require "simplecov"
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "keep_up"
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = "spec/examples.txt"
+end
