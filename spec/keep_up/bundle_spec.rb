9df3ac5cfc99b8a084263e17f4aa410aa8c65710
Matijs van Zuijlen <matijs@matijs.net>
2024-10-27 12:26:43 +0100

    Replace mocking in Bundle specs with full setup of bundle and gem index

--- spec/keep_up/bundle_spec.rb
+++ spec/keep_up/bundle_spec.rb
@@ -2,27 +2,44 @@
 
 require "spec_helper"
 
-describe KeepUp::Bundle do
-  let(:runner) { class_double KeepUp::Runner }
+describe KeepUp::Bundle, type: :aruba do
+  let(:runner) { KeepUp::Runner }
   let(:bundle) do
     described_class.new runner: runner, local: false
   end
 
-  describe "#outdated_dependencies" do
-    let(:outdated_result) do
-      "\n" \
-        "foo (newest 0.1.0, installed 0.0.5)\n" \
-        "bar (newest 0.2.1, installed 0.1.5, requested ~> 0.1.2)\n" \
-        "baz (newest 1.2.1 f1e2d3c, installed 0.2.5 dbabdab)\n" \
-        "qux (newest 0.3.4, installed 0.2.8, requested > 0.1.2, <= 0.3.0)\n"
+  before do
+    write_local_gemfile("gemspec")
+    write_gemspec("qux", "0.1.0", { "foo" => nil,
+                                    "bar" => "~> 0.1.2",
+                                    "baz" => ["> 0.1.2", "<= 0.3.0"] })
+    write_gem("foo", "0.0.5")
+    write_gem("bar", "0.1.5")
+    write_gem("baz", "0.2.8")
+
+    generate_local_gem_index
+
+    in_current_directory { Bundler.with_unbundled_env { `bundle lock` } }
+
+    write_gem("foo", "0.1.0")
+    write_gem("bar", "0.2.1")
+    write_gem("baz", "0.3.4")
+
+    generate_local_gem_index
+
+    in_current_directory do
+      Bundler.with_unbundled_env { `bundle config set path 'vendor'` }
     end
+  end
+
+  describe "#outdated_dependencies" do
     let(:expected_dependencies) do
       [
         KeepUp::Dependency.new(
           name: "foo",
           locked_version: "0.0.5",
           newest_version: "0.1.0",
-          requirement_list: nil
+          requirement_list: [">= 0"]
         ),
         KeepUp::Dependency.new(
           name: "bar",
@@ -32,12 +49,6 @@ describe KeepUp::Bundle do
         ),
         KeepUp::Dependency.new(
           name: "baz",
-          locked_version: "0.2.5",
-          newest_version: "1.2.1",
-          requirement_list: nil
-        ),
-        KeepUp::Dependency.new(
-          name: "qux",
           locked_version: "0.2.8",
           newest_version: "0.3.4",
           requirement_list: ["> 0.1.2", "<= 0.3.0"]
@@ -45,64 +56,41 @@ describe KeepUp::Bundle do
       ]
     end
 
-    before do
-      allow(runner).to receive(:run).and_return(outdated_result)
-    end
-
-    it "runs bundle outdated with parseable results" do
-      bundle.outdated_dependencies
-      expect(runner).to have_received(:run).with("bundle outdated --parseable")
-    end
-
     it "returns the correct set of dependencies" do
-      result = bundle.outdated_dependencies
-      expect(result).to eq expected_dependencies
+      result = in_current_directory do
+        Bundler.with_unbundled_env { bundle.outdated_dependencies }
+      end
+      expect(result).to match_array expected_dependencies
     end
   end
 
   describe "#update_dependency" do
     let(:dependency) do
-      KeepUp::Dependency.new(name: "bar", locked_version: "1.0.0", newest_version: "2.0.0",
-                             requirement_list: [])
+      KeepUp::Dependency.new(
+        name: "bar",
+        locked_version: "0.1.5",
+        newest_version: "0.2.1",
+        requirement_list: ["~> 0.1.2"]
+      )
     end
 
-    before do
-      allow(runner).to receive(:run).and_return(run_result)
-    end
-
-    context "when bundler shows the old version" do
-      let(:run_result) do
-        <<~OUTPUT
-          Using foo 1.0.1
-          Using bar 2.0.0 (was 1.0.0)
-          Using baz 3.2.1
-        OUTPUT
+    it "returns the correct lockfile update" do
+      _, lock_result = in_current_directory do
+        Bundler.with_unbundled_env { bundle.update_dependency(dependency) }
       end
-
-      it "detects the lockfile update by comparing with the old version" do
-        _, lock_result = bundle.update_dependency(dependency)
-        aggregate_failures do
-          expect(lock_result.name).to eq "bar"
-          expect(lock_result.version).to eq Gem::Version.new("2.0.0")
-        end
+      aggregate_failures do
+        expect(lock_result.name).to eq "bar"
+        expect(lock_result.version).to eq Gem::Version.new("0.2.1")
       end
     end
 
-    context "when bundler does not show the old version" do
-      let(:run_result) do
-        <<~OUTPUT
-          Using foo 1.0.1
-          Using bar 2.0.0
-          Using baz 3.2.1
-        OUTPUT
+    it "returns the correct specfile update" do
+      spec_result, = in_current_directory do
+        Bundler.with_unbundled_env { bundle.update_dependency(dependency) }
       end
-
-      it "detects the lockfile update by comparing with the old version" do
-        _, lock_result = bundle.update_dependency(dependency)
-        aggregate_failures do
-          expect(lock_result.name).to eq "bar"
-          expect(lock_result.version).to eq Gem::Version.new("2.0.0")
-        end
+      aggregate_failures do
+        expect(spec_result.name).to eq "bar"
+        expect(spec_result.version).to eq Gem::Version.new("0.2.1")
       end
     end
   end
